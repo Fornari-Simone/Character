910d570a012d8e7a042869a62f3872f134ea3409
Fornari-Simone <20simone02@gmail.com>
2024-02-10 17:08:15 +0100

    adj112

---
+++ Character.API/Migrations/20240210104013_TransactionalOutbox.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Character.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionalOutbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransitionalOutboxes",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tabella = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Messaggio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransitionalOutboxes", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransitionalOutboxes");
+        }
+    }
+}
